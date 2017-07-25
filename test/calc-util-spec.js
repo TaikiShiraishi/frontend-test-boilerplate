69536dda12e499ffa52f3685b9972c284391778c
TaikiShiraishi <tiki.t.s0118@gmail.com>
2017-07-25 19:43:45 +0900

    Refactor ES6

--- test/calc-util-spec.js
+++ test/calc-util-spec.js
@@ -1,12 +1,12 @@
-var assert = require('power-assert')
-var calcUtil = require('../src/calc-util.js');
+import assert from 'power-assert';
+import add  from '../src/calc-util.js';
 
 describe('add関数のテスト', function() {
   it('1+2は3である', function() {
-    assert(calcUtil.add(1, 2) === 3)
+    assert(add(1, 2) === 3)
   });
   it('1+2は4である', function() {
-    assert(calcUtil.add(1, 2) === 4)
+    assert.equal(add(1, 2), 4)
   });
 });
 
