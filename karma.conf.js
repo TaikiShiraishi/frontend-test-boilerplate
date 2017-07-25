b6ea2e32b6cd91d44aac261e0ef3f34e37022505
TaikiShiraishi <tiki.t.s0118@gmail.com>
2017-07-25 20:43:34 +0900

    Remove line

--- karma.conf.js
+++ karma.conf.js
@@ -12,17 +12,14 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
 
-
     // list of files / patterns to load in the browser
     files: [
       __dirname + '/test/**/*.js'
     ],
 
-
     // list of files to exclude
     exclude: [
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
