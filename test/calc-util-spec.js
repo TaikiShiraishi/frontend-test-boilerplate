4a7059eec519e7cc57caba487689404f26073e91
TaikiShiraishi <tiki.t.s0118@gmail.com>
2017-07-25 19:26:56 +0900

    Replace assert plugin

--- test/calc-util-spec.js
+++ test/calc-util-spec.js
@@ -1,16 +1,12 @@
-var assert = require('assert');
+var assert = require('power-assert')
 var calcUtil = require('../src/calc-util.js');
 
 describe('add関数のテスト', function() {
   it('1+2は3である', function() {
-    if (calcUtil.add(1, 2) === 3) {
-      assert(calcUtil.add(1, 2) === 3)
-    }
+    assert(calcUtil.add(1, 2) === 3)
   });
   it('1+2は4である', function() {
-    if (calcUtil.add(1, 2) === 4) {
-      assert(calcUtil.add(1, 2) === 4)
-    }
+    assert(calcUtil.add(1, 2) === 4)
   });
 });
 
