ca961f97efcbafde3cfb8a1885ee1bb4962bd08a
TaikiShiraishi <tiki.t.s0118@gmail.com>
2017-07-25 19:42:41 +0900

    Add compile babel setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,22 @@
 module.exports = {
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['babel-plugin-espower']
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
+  },
   entry: {
     app: './src/index.js'
   },
